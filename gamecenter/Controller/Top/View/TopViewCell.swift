95f0e3d952812629625beb8766992f760c0fe555
MLH-Đào Xuân Vũ <57245417+mlh-vudx@users.noreply.github.com>
2020-10-09 11:37:42 +0700

    commit

--- gamecenter/Controller/Top/View/TopViewCell.swift
+++ gamecenter/Controller/Top/View/TopViewCell.swift
@@ -396,11 +396,3 @@ extension AutoPlayVideoLayerContainer where Self: UICollectionViewCell {
         VideoPlayerController.shared.pauseVideosFor(cell: self)
     }
 }
-
-extension Double {
-    /// Rounds the double to decimal places value
-     func rounded(toPlaces places:Int) -> Double {
-           let divisor = pow(10.0, Double(places))
-           return (self * divisor).rounded() / divisor
-       }
-}
